9c6572902da97ef736a4548a72f8aed8d5a634d1
Antony Kellermann <aokellermann@gmail.com>
2020-12-25 17:04:27 -0500

    Add setup.py to format/lint

--- scripts/format.sh
+++ scripts/format.sh
@@ -3,9 +3,9 @@
 set -eo pipefail
 
 if [ "$#" -eq 0 ]; then
-	yapf -i -r gym_minesweeper 2>/dev/null
+	yapf -i -r gym_minesweeper setup.py 2>/dev/null
 	shfmt -w scripts
 elif [ "$1" == "check" ]; then
-	yapf -d -r gym_minesweeper
+	yapf -d -r gym_minesweeper setup.py
 	shfmt -l -d scripts
 fi
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 
-pylint --rcfile=setup.cfg gym_minesweeper
+pylint --rcfile=setup.cfg gym_minesweeper setup.py
 shellcheck scripts/hooks/* scripts/*.sh
