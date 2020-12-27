08f25bbe05d2ad6209d396247d03451da30c728d
Antony Kellermann <aokellermann@gmail.com>
2020-12-26 21:23:37 -0500

    lint

--- gym_minesweeper/__init__.py
+++ gym_minesweeper/__init__.py
@@ -6,6 +6,7 @@ __all__ = [
 
 from gym.envs.registration import register
 
-from gym_minesweeper.minesweeper import MinesweeperEnv, SPACE_MINE, SPACE_UNKNOWN, DEFAULT_REWARD_WIN, DEFAULT_REWARD_LOSE, DEFAULT_REWARD_CLEAR
+from gym_minesweeper.minesweeper import MinesweeperEnv, SPACE_MINE, SPACE_UNKNOWN, DEFAULT_REWARD_WIN,\
+    DEFAULT_REWARD_LOSE, DEFAULT_REWARD_CLEAR
 
 register(id='Minesweeper-v0', entry_point='gym_minesweeper:MinesweeperEnv')
